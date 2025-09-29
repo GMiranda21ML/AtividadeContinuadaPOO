a001ef1a77620fada290331c6ff5e5c2325b1d74
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-09-29 14:31:42 -0300

    enviando arquivos

---
+++ src/br/edu/cs/poo/ac/ordem/mediators/ClienteMediator.java
@@ -0,0 +1,11 @@
+package br.edu.cs.poo.ac.ordem.mediators;
+
+public class ClienteMediator {
+	public ResultadoMediator incluir(Cliente cliente) {
+		cliente.
+	}
+	public ResultadoMediator alterar(Cliente cliente) {}
+	public ResultadoMediator excluir(String cpfCnpj) {}
+	public Cliente buscar(String cpfCnpj) {}
+	public ResultadoMediator validar(Cliente cliente) {}
+}
---
+++ src/br/edu/cs/poo/ac/utils/StringUtils.java
@@ -0,0 +1,65 @@
+package br.edu.cs.poo.ac.utils;
+
+public class StringUtils {
+	public static boolean estaVazia(String str) {
+		if (str == null) {
+			return true;
+		}
+		
+		if (str.isEmpty()) {
+			return true;
+		}
+		
+		if (str.isBlank()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean tamanhoExcedido(String str, int tamanho) {
+		if (tamanho < 0) {
+			return false;
+		}
+		
+		if (str == null && tamanho == 0) {
+			return false;
+		} else if (str == null && tamanho > 0) {
+			return true;
+		}
+		
+		if (str.length() == tamanho) {
+			return false;
+		}
+		
+		if (str.length() < tamanho) {
+			return false;
+		}
+				
+		return true;
+	}
+	
+	public static boolean emailValido(String email) {
+		if (email == null) {
+			return false;
+		}
+		
+		if (email.contains("@")) {
+			return true;
+		} 
+		
+		return false;
+	}
+	
+	public static boolean telefoneValido(String tel) {
+		if (tel == null) {
+			return false;
+		}
+		
+		if (tel.contains("(") && tel.contains(")") && (tel.length() == 12 || tel.length() == 13)) {
+			return true;
+		}
+		
+		return false;
+	}
+}
